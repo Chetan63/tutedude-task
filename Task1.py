86444e1f089aa127e965d39325ad4ba322bdc10e
Chetan63 <115644102+Chetan63@users.noreply.github.com>
2025-06-22 22:02:38 +0530

    Add files via upload

---
+++ Task1.py
@@ -0,0 +1,16 @@
+# Task 1
+
+# input first number
+a=int(input("enter a number:"))
+# input second number
+b=int(input("enter another number:"))
+#operations
+c = (a+b) #addition
+d = (a-b) #subtraction
+e = (a*b) #multiplication
+f = int(a/b) #division
+print("the addition is:" ,c)
+print("the subtraction is:" ,d)
+print("the multiplication is:" ,e)
+print("the division is:" ,f)
+
---
+++ Task2.py
@@ -0,0 +1,7 @@
+# task2
+
+a1=input("enter the first name: ")
+a2=input("enter the second name: ")
+a3 = (a1+a2)
+a4 = 'Hello '
+print(a4 + a3, ", Welcome to the palace")
