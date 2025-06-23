349bf58ac4143c45b79886d1aa2025ab30d77d0c
Chetan63 <115644102+Chetan63@users.noreply.github.com>
2025-06-23 21:59:04 +0530

    Add files via upload

---
+++ Assignment 2 task 1.py
@@ -0,0 +1,6 @@
+# even or odd
+n=int(input("Enter a number to be check: "))
+if n%2==0:
+    print("Even")
+else:
+    print("Odd")
---
+++ Assignment 2 task 2.py
@@ -0,0 +1,6 @@
+# sum using loop
+total_sum = 0
+n= int(input("Enter the range: "))
+for i in range(0,n+1):
+    total_sum += i
+print( "the total sum is : ", total_sum)
